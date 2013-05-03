ab09565f87490cc1b9ac54836ac02c80f93ca974
mryoshio <ashitaba.yoshioka@leonisand.co>
2013-05-03 18:47:47 +0900

    add some fixes

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
-from google.appengine.api import memcache
-from model import Opinion
+from google.appengine.api import memcache, users
+from google.appengine.ext.db import Key
+
+from model import Opinion, Group
 
 import webapp2
 import cgi, jinja2, logging, os
@@ -9,18 +11,32 @@ jinja2_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.d
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    c = memcache.get('top')
-    if c == None:
-      opinions = Opinion.gql("WHERE archived = :1 ORDER BY vote DESC, created DESC", False)
-      template_values = { 'opinions': opinions }
-      template = jinja2_environment.get_template('index.html')
-      t = template.render(template_values)
-      memcache.set('top', t, time=3600)
-      self.response.out.write(t)
+    user = users.get_current_user()
+    if user:
+      u_link = ('<a href=\"%s\">Sign out</a>' % users.create_logout_url('/'))
+      c = memcache.get('top')
+      if c == None:
+        opinions = Opinion.gql("WHERE archived = :1 ORDER BY vote DESC, created DESC", False)
+        template_values = { 'opinions' : opinions, 'u_link' : u_link }
+        template = jinja2_environment.get_template('html/index.html')
+        t = template.render(template_values)
+        memcache.set('top', t, time=3600)
+        self.response.out.write(t)
+      else:
+        self.response.out.write(c)
     else:
-      self.response.out.write(c)
+      u_link = ('<a href=\"%s\">sign in</a>' % users.create_login_url('/'))
+      self.response.out.write('<html><body>%s</body></html>' % u_link)
 
 class OpinionPage(webapp2.RequestHandler):
+  def get(self):
+    g = Group.get(self.request.get('g'))
+    opinions = Opinion.gql("WHERE group = :1", g)
+    template_values = { 'opinions' : opinions, 'g' : g }
+    template = jinja2_environment.get_template('html/opinion.html')
+    t = template.render(template_values)
+    self.response.out.write(t)
+
   def _put(self):
     o = Opinion.get(self.request.get('_k'))
     if self.request.get('_t') == 'v':
@@ -36,20 +52,38 @@ class OpinionPage(webapp2.RequestHandler):
       o.delete()
 
   def post(self):
+    print 'aaaaaaaa'
     if self.request.get('_m') == 'put':
       self._put()
     elif self.request.get('_m') == 'delete':
       self._delete()
     else:
       o = Opinion(
-        title=cgi.escape(self.request.get('title')),
-        description=cgi.escape(self.request.get('description')),
-        nickname=cgi.escape(self.request.get('nickname'))
+        title = cgi.escape(self.request.get('title')),
+        description = cgi.escape(self.request.get('description')),
+        author = users.get_current_user(),
+        group = Key(self.request.get('g'))
         )
       o.put()
     memcache.delete('top')
     self.redirect('/')
 
+class GroupPage(webapp2.RequestHandler):
+  def get(self):
+    template = jinja2_environment.get_template('html/group.html')
+    gs = Group.all()
+    template_values = { 'groups' : gs }
+    t = template.render(template_values)
+    self.response.out.write(t)
+    
+  def post(self):
+    g = Group(
+      name=cgi.escape(self.request.get('name')),
+      owner=users.get_current_user()
+      )
+    g.put()
+    self.redirect('/groups')
+    
 class TaskPage(webapp2.RequestHandler):
   def get(self):
     if (self.request.get('t') == 'update_schema'):
@@ -57,6 +91,6 @@ class TaskPage(webapp2.RequestHandler):
       for o in opinions:
         o.put()
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/opinions', OpinionPage), ('/tasks', TaskPage)],
+app = webapp2.WSGIApplication([('/', MainPage), ('/opinions', OpinionPage), ('/groups', GroupPage), ('/tasks', TaskPage)],
                               debug=True)
 
--- model.py
+++ model.py
@@ -1,15 +1,16 @@
 from google.appengine.ext import db
 
 
+class Group(db.Model):
+    name = db.StringProperty(required=True)
+    owner = db.UserProperty(required=True)
+
 class Opinion(db.Model):
     title = db.StringProperty(required=True)
     description = db.StringProperty(required=True, multiline=True)
     vote = db.IntegerProperty(required=True, default=0)
-    author = db.StringProperty()
-    nickname = db.StringProperty()
+    author = db.UserProperty(required=True)
+    group = db.ReferenceProperty(Group)
     archived = db.BooleanProperty(default=False)
-    created = db.DateTimeProperty(auto_now_add=True)
-    updated = db.DateTimeProperty(auto_now_add=True)
-
-class Group(db.Model):
-    pass
+    created_at = db.DateTimeProperty(auto_now_add=True)
+    updated_at = db.DateTimeProperty(auto_now_add=True)
